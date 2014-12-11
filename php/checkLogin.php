5ab33cdbd9ddc9db8f6907b7f6189ed8b031ba84
unknown <Julian@.(none)>
2014-12-11 18:05:05 +0100

    Login Commit

--- php/checkLogin.php
+++ php/checkLogin.php
@@ -4,6 +4,14 @@ if (!isset($_SESSION)) {
 }
 if (isset($_SESSION['Login'])) {
 	if ($_SESSION['Login'] == 1) {
+			include('dao.php');
+	$employer = new EmployerDAO();
+	$employer->connect();
+	$result=$employer->getEntry($_SESSION['pernr']);
+		echo '<li>';
+		echo '<p class="navbar-text">Signed in as '.$result['forname'].' '.$result['lastname'].'</p>';
+		echo '</li>';
+		echo '<li>';
 		$click = '../php/logout.php';
 		echo '<butto class="btn btn-primary navbar-btn" onclick=location.href="' . $click . '">';
 		echo '<i class="glyphicon glyphicon-off"></i> Logout</button>';
@@ -11,6 +19,10 @@ if (isset($_SESSION['Login'])) {
 
 	}
 } else {
+	echo '<li>';
+	echo '<p class="navbar-text">Not signed in</p>';
+	echo '</li>';
+	echo '<li>';
 	$click = 'login.html';
 	echo '<butto class="btn btn-primary navbar-btn" onclick=location.href="' . $click . '">';
 	echo '<i class="glyphicon glyphicon-off"></i> Login</button>';
--- php/pdf.php
+++ php/pdf.php
@@ -3,50 +3,54 @@ if (isset($_POST['calc'])) {
 	include 'dao.php';
 	$entry = new EntryDAO();
 	$entry -> connect();
-	echo $data[] = $_POST['month'];
-	echo $data[] = $_POST['year'];
-	echo $data[] = $_POST['p_id'];
 	$result = $entry -> getMonth($data);
-	try {
-		$p = new PDFlib();
+	if (!empty($result)) {
+		try {
+			$p = new PDFlib();
 
-		/* öffnet eine neue PDF-Datei; fügen Sie einen Dateinamen ein,
-		 um das PDF auf der Platte zu speichern */
-		if ($p -> begin_document("", "") == 0) {
-			die("Error: " . $p -> get_errmsg());
+			/* öffnet eine neue PDF-Datei; fügen Sie einen Dateinamen ein,
+			 um das PDF auf der Platte zu speichern */
+			if ($p -> begin_document("", "") == 0) {
+				die("Error: " . $p -> get_errmsg());
+			}
+
+			$p -> set_info("Creator", "ActivityRecording");
+			$p -> set_info("Author", "Rainer Schaaf");
+			$p -> set_info("Title", "Hallo Welt (PHP)!");
+
+			$p -> begin_page_ext(595, 842, "");
+
+			$font = $p -> load_font("Helvetica-Bold", "winansi", "");
+
+			$p -> setfont($font, 24.0);
+			$p -> set_text_pos(50, 700);
+			$p -> show("Abrechnung " . $_POST['month'] . ' ' . $_POST['year']);
+			foreach ($result as $row) {
+
+				$p -> continue_text($row['dates'] . ' ' . $row['cost_type'] . ' ' . $row['job'] . ' ' . $row['hours']);
+			}
+			$p -> end_page_ext("");
+
+			$p -> end_document("");
+
+			$buf = $p -> get_buffer();
+			$len = strlen($buf);
+
+			header("Content-type: application/pdf");
+			header("Content-Length: $len");
+			header("Content-Disposition: inline; filename=Abrechnung.pdf");
+			print $buf;
+		} catch (PDFlibException $e) {
+			die("Eine PDFlib-Exception ist aufgetreten im hallo-Schnipsel:\n" . "[" . $e -> get_errnum() . "] " . $e -> get_apiname() . ": " . $e -> get_errmsg() . "\n");
+		} catch (Exception $e) {
+			die($e);
 		}
+		$p = 0;
 
-		$p -> set_info("Creator", "ActivityRecording");
-		$p -> set_info("Author", "Rainer Schaaf");
-		$p -> set_info("Title", "Hallo Welt (PHP)!");
-
-		$p -> begin_page_ext(595, 842, "");
-
-		$font = $p -> load_font("Helvetica-Bold", "winansi", "");
-
-		$p -> setfont($font, 24.0);
-		$p -> set_text_pos(50, 700);
-		$p -> show("Abrechnung " . $_POST['month'] . ' ' . $_POST['year']);
-		foreach ($result as $row) {
-
-			$p -> continue_text($row['dates'].' '.$row['cost_type']. ' '.$row['job'].' '.$row['hours']);
-		}
-		$p -> end_page_ext("");
-
-		$p -> end_document("");
-
-		$buf = $p -> get_buffer();
-		$len = strlen($buf);
-
-		header("Content-type: application/pdf");
-		header("Content-Length: $len");
-		header("Content-Disposition: inline; filename=Abrechnung.pdf");
-		print $buf;
-	} catch (PDFlibException $e) {
-		die("Eine PDFlib-Exception ist aufgetreten im hallo-Schnipsel:\n" . "[" . $e -> get_errnum() . "] " . $e -> get_apiname() . ": " . $e -> get_errmsg() . "\n");
-	} catch (Exception $e) {
-		die($e);
 	}
-	$p = 0;
+else {
+	echo 'alert("no entrys in this period")';
+	header("Location: ../html/accounting.html");
+}
 }
 ?>
