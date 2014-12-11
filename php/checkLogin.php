204934d454ce3853a9e33b48acfe5bf50f176b10
Stefan <stefanbecking@gmail.com>
2014-12-11 17:59:00 +0100

    some fixes always redirect to login page when not logged in

--- php/checkLogin.php
+++ php/checkLogin.php
@@ -4,15 +4,19 @@ if (!isset($_SESSION)) {
 }
 if (isset($_SESSION['Login'])) {
 	if ($_SESSION['Login'] == 1) {
+		echo "<li>";
+		echo "<p class='navbar-text'>Logged in as " . $_SESSION['Mail'] . "</p>";
+		echo "</li>";
+		echo "<li>";
 		$click = '../php/logout.php';
 		echo '<butto class="btn btn-primary navbar-btn" onclick=location.href="' . $click . '">';
 		echo '<i class="glyphicon glyphicon-off"></i> Logout</button>';
-	} else {
-
+		echo "</li>";
 	}
 } else {
-	$click = 'login.html';
-	echo '<butto class="btn btn-primary navbar-btn" onclick=location.href="' . $click . '">';
-	echo '<i class="glyphicon glyphicon-off"></i> Login</button>';
+	header("Location: ../html/login.html"); // not logged in --> always redirect to login page
+// 	$click = 'login.html';
+// 	echo '<butto class="btn btn-primary navbar-btn" onclick=location.href="' . $click . '">';
+// 	echo '<i class="glyphicon glyphicon-off"></i> Login</button>';
 }
 ?>
